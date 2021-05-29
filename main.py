feb3301c210760b0de848aafc1ad2cb9052fc878
github-HDC <68763321+github-HDC@users.noreply.github.com>
2021-05-29 12:27:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from youdao import *
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 
@@ -21,3 +21,5 @@ def read_root(
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+##假如需要放在阿里云等云服务器供外部访问，需要将host改为 '0.0.0.0'
+uvicorn.run(app,host='127.0.0.1',port=8000)    
